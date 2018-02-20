b802b157c4bdc51e083c1f896ff7046d9e7a1109
Gaurav Bajaj <gbajaj@apple.com>
2018-02-20 12:09:06 -0800

    Program to extract top 10 records

--- Musicdataset/musicds.py
+++ Musicdataset/musicds.py
@@ -5,7 +5,7 @@ The data is loaded into HDFS using hadoop fs -mkdir and hdfs cp comamnds
 resides under dir named IN under hdfs i.e IN/reviews_Digital_Music_5.json.
 This program reads the Data in json format ,  create RDD,datasets out of it 
 Data is in json format named "reviews_Digital_Music_5.json" which is feeded in hdfs 
-python prog musicds.py extract and display the required result i.e  top ten songs
+python prog musicds.py extract and display the required result i.e  top ten songs'''
 
 To run:
    - PYSPARK_PYTHON=/home/common/conda/anaconda2/bin/python \ 
