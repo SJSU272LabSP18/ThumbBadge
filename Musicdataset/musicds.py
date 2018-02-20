cd3deecfff9d2bb94ea7ad22235789146c865e65
Gaurav Bajaj <gbajaj@apple.com>
2018-02-19 16:50:47 -0800

    Program to extract top 10 records

---
+++ Musicdataset/musicds.py
@@ -0,0 +1,13 @@
+#Spark use-case22 Music dataset
+# The data is loaded into HDFS using hadoop fs -mkdir and hdfs put comamnds##
+from pyspark.sql import SparkSession
+spark = SparkSession \
+    .builder \
+    .appName("Python Spark SQL Music datasets") \
+    .config("spark.some.config.option", "some-value") \
+    .getOrCreate()
+df = spark.read.json("In/reviews_Digital_Music_5.json")
+df.printSchema()
+df.createOrReplaceTempView("music")
+sqlDF = spark.sql("SELECT summary,overall  FROM music WHERE overall = 5.0 LIMIT 10")
+sqlDF.show()
